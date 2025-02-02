aaa004c999f5a4416aaf6610777f7f06e124be47
taksqth <gabriel.takeshi.higa@gmail.com>
2025-02-01 21:14:59 -0300

    fix: navigation for /terms and /privacy was not working

--- tier-synthesis/main.py
+++ tier-synthesis/main.py
@@ -103,6 +103,7 @@ def get_privacy(htmx):
     content = Titled(
         "Privacy Policy",
         Zero_md(css_template, Script(md_content, type="text/markdown")),
+        id="main",
     )
     if htmx.request is None:
         return get_full_layout(content)
@@ -117,6 +118,7 @@ def get_privacy(htmx):
     content = Titled(
         "Terms of Service",
         Zero_md(css_template, Script(md_content, type="text/markdown")),
+        id="main",
     )
     if htmx.request is None:
         return get_full_layout(content)
