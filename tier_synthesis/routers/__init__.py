2043888667a916dd98f2b9eecd7c5298eb7b5322
Gabriel Takeshi Andrade Higa <gabriel.higa@yduqs.com.br>
2025-11-02 16:37:11 -0300

    [hotfix] fix import order

--- tier_synthesis/routers/__init__.py
+++ tier_synthesis/routers/__init__.py
@@ -1,7 +1,7 @@
 def get_api_routers():
-    from .images_router import images_router
     from .users_router import users_router
-    from .tierlist_router import tierlist_router
     from .groups_router import groups_router
+    from .images_router import images_router
+    from .tierlist_router import tierlist_router
 
     return [images_router, tierlist_router, users_router, groups_router]
